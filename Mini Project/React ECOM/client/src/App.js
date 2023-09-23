5152d69b594057373a9608957e136c30a8317e08
Shon G <122247368+shon-1@users.noreply.github.com>
2023-09-24 05:05:27 +0530

    +Routes
    Connectivity in react

--- Mini Project/React ECOM/client/src/App.js
+++ Mini Project/React ECOM/client/src/App.js
@@ -1,12 +1,23 @@
-import "./App.css";
-import Layout from "./components/layout/Layout";
+import { renderMatches } from "react-router-dom";
+import { Routes,Route} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import PageNotFound from "./pages/PageNotFound";
+import Policy from "./pages/Policy";
+// Routes like container
 function App() {
   return (
     <>
-      <Layout>
-        <h1> Ecommerse APP</h1>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/Contact' element={<Contact/>}/>
+      <Route path='/Policy' element={<Policy/>}/>
+      <Route path='/*' element={<PageNotFound/>}/>
+    </Routes>
         
-      </Layout>
+ 
     </>
   );
 }
