0646850ffe980ed97f6df56e59cefc333a285bee
Shon G <122247368+shon-1@users.noreply.github.com>
2023-09-24 04:47:12 +0530

    +layout
    +Footer setuped

---
+++ Mini Project/React ECOM/client/src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-3">
+      <h3  className="text-center">All Right Reserved &copy; KDRAMA WORLD</h3>
+      <p className="text-center mt-3">
+        {/* <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
+        <Link to="/policy">Privacy Policy</Link>*/}
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
