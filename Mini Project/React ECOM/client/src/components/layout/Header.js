e06c11bf56a5799b3f9bfe63f46afa5a454288ba
Shon G <122247368+shon-1@users.noreply.github.com>
2023-09-30 06:37:21 +0530

    +admin/user route
    +section

--- Mini Project/React ECOM/client/src/components/layout/Header.js
+++ Mini Project/React ECOM/client/src/components/layout/Header.js
@@ -1,95 +1,64 @@
 import React from "react";
-import { NavLink, Link } from "react-router-dom";
+import { NavDropdown, Navbar, Nav } from "react-bootstrap";
 import { GrRestaurant } from 'react-icons/gr';
 import { AiFillHome } from 'react-icons/ai';
 import { BiSolidLogIn } from 'react-icons/bi';
 import { useAuth } from "../../context/auth";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [auth, setAuth] = useAuth();
 
   const handleLogout = () => {
-    localStorage.removeItem('auth')
-
+    localStorage.removeItem('auth');
     setAuth({
       ...auth,
       user: null,
       token: "",
-      //localStorage:removeItem('token'),
-
     });
-
   };
+
   return (
-    <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <Link to="/" className="navbar-brand">
-              <GrRestaurant />🥘 Good - food
-
-            </Link>
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <NavLink to="/" className="nav-link ">
-                  <AiFillHome /> Home
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/RegLog" className="nav-link">
-                  Category
-                </NavLink>
-              </li>
-
-              {
-                !auth.user ? (
-
-                  <>
-                    <li className="nav-item">
-                      <NavLink to="/register" className="nav-link">
-                        Register
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink to="/Login" className="nav-link">
-                        <BiSolidLogIn /> Login
-                      </NavLink>
-                    </li>
-                  </>
-
-                ) : (
-                  <>
-                    <li className="nav-item">
-                      <NavLink onClick={handleLogout} to="/Login" className="nav-link">
-                        <BiSolidLogIn /> Logout
-                      </NavLink>
-                    </li>
-
-                  </>
-                )
-
-              }
-              <li className="nav-item">
-                <NavLink to="/dashboard" className="nav-link">
-                  Cart (0)
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
+    <Navbar bg="body-tertiary" expand="lg">
+      <div className="container-fluid">
+        <Navbar.Toggle aria-controls="navbarTogglerDemo01" />
+        <Navbar.Collapse id="navbarTogglerDemo01">
+          <Link to="/" className="navbar-brand">
+            <GrRestaurant />🥘 Good - food
+          </Link>
+          <Nav className="ms-auto mb-2 mb-lg-0">
+            <Nav.Link as={Link} to="/">
+              <AiFillHome /> Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/RegLog">
+              Category
+            </Nav.Link>
+            {!auth.user ? (
+              <>
+                <Nav.Link as={Link} to="/register">
+                  Register
+                </Nav.Link>
+                <Nav.Link as={Link} to="/login">
+                  <BiSolidLogIn /> Login
+                </Nav.Link>
+              </>
+            ) : (
+              <NavDropdown title={auth?.user?.name} id="basic-nav-dropdown">
+    <NavDropdown.Item as={Link} to={`/dashboard/${auth?.user?.role === 1 ? "admin" : "user"}`}>
+      Dashboard
+    </NavDropdown.Item>
+    <NavDropdown.Item onClick={handleLogout} as={Link} to="/login">
+      Logout
+    </NavDropdown.Item>
+  </NavDropdown>
+            )}
+            <Nav.Link as={Link} to="/dashboard">
+              Cart (0)
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </div>
+    </Navbar>
   );
 };
 
