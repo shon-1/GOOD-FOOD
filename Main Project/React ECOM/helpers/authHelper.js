62ab3e8081600eb301897391cf28660592170c18
Shon G <122247368+shon-1@users.noreply.github.com>
2024-04-17 09:10:08 +0530

    +hashingUPDATE

--- Main Project/React ECOM/helpers/authHelper.js
+++ Main Project/React ECOM/helpers/authHelper.js
@@ -1,21 +1,15 @@
-import argon2 from 'argon2';
+import bcrypt from 'bcrypt'
 
-export const hashPassword = async (password) => {
+export const hashPassword = async(password) => {
     try {
-        const hashedPassword = await argon2.hash(password);
-        return hashedPassword;
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password,saltRounds);
+        return hashedPassword
     } catch (error) {
         console.log(error);
-        throw new Error('Failed to hash password');
     }
 };
 
-export const comparePassword = async (password, hashedPassword) => {
-    try {
-        const isMatch = await argon2.verify(hashedPassword, password);
-        return isMatch;
-    } catch (error) {
-        console.log(error);
-        throw new Error('Failed to compare passwords');
-    }
-};
+export const comparePassword = async (password,hashedPassword) => {
+    return bcrypt.compare(password,hashedPassword);
+}
